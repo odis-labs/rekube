9dad20ee7fe006490002466fda1d5966622c4d3c
Rizo Isrof <rizo@odis.io>
2019-03-18 15:34:27 +0000

    Error fmt in ppx.

--- ppx/Main.ml
+++ ppx/Main.ml
@@ -8,7 +8,7 @@ open Longident
 
 
 let fail loc s =
-  raise (Location.Error (Location.error ~loc ("rekube.ppx: " ^ s)))
+  raise (Location.Error (Location.error ~loc ("[ERROR] " ^ s)))
 
 
 let mk_func ~loc longident =
@@ -57,7 +57,7 @@ let rec expr mapper e =
   | Pexp_extension (
       {txt="bs.obj"; _},
       PStr [{pstr_desc = Pstr_eval({pexp_desc=Pexp_record (_fields, None); _}, _); _}]
-    ) -> fail e.pexp_loc "Error: Object literal without annotation."
+    ) -> fail e.pexp_loc "Object literal without annotation."
 
   | _ -> default_mapper.expr mapper e
 
